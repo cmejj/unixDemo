c702e206f40089ebcbe5dfc70927e55937203894
kingchin1218 <kingchin1218@126.com>
2016-08-14 08:16:51 -0700

    a demo that likes shell
    we known that priciple of shell by this demo

---
+++ demo2/demo2.c
@@ -0,0 +1,37 @@
+#include "apue.h"
+#include <sys/wait.h>
+
+int main(){
+
+	char	buf[MAXLINE];	//from apue.h
+	pid_t	pid;
+	int	status;
+
+	printf("%% ");		/*print prompt (printf requirs %% to print %)*/
+	while(fgets(buf,MAXLINE,stdin) != NULL){
+		if(buf[strlen(buf) - 1] == '\n'){
+			buf[strlen(buf) - 1] = 0; /*replace new line with null*/
+		}
+		
+		
+		if((pid = fork()) < 0){
+			err_sys("fork error");
+		}
+		else if(pid == 0){	/*child process*/
+			execlp(buf, buf, (char*) 0);
+			err_ret("coludn't execute: %s",buf);
+			exit(127);
+		}
+
+		
+		/*parent's process*/
+		if((pid = waitpid(pid, &status, 0)) < 0){
+			err_sys("waitpid error");
+		}
+		printf("%% ");
+
+	}
+	exit(0);
+
+	return 0;
+}
